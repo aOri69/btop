b01ed378508cf4fbf3e06579a17a8784a1e0b856
Aleksandr Sokolov <sashkaptits@gmail.com>
2023-08-21 13:44:50 +0400

    Inintial commit manualy copied from local repo

---
+++ src/app.rs
@@ -0,0 +1,118 @@
+use std::collections::VecDeque;
+
+#[derive(Default)]
+pub struct App {
+    pub percentage: f64,
+    pub vendor: String,
+    pub power: battery::units::Power,
+    pub energy: battery::units::Energy,
+    pub time_to_empty: battery::units::Time,
+    pub time_to_full: battery::units::Time,
+    pub temperature: battery::units::ThermodynamicTemperature,
+    pub voltage: battery::units::ElectricPotential,
+    pub cycle_count: u32,
+    pub model: String,
+    pub serial_number: String,
+    pub state: battery::State,
+    pub state_of_health: f32,
+    pub technology: battery::Technology,
+    pub power_bar: VecDeque<f64>,
+    pub power_bar_y_max: f64,
+    pub power_bar_y_min: f64,
+}
+
+impl App {
+    pub fn new() -> App {
+        App {
+            percentage: f64::default(),
+            vendor: String::new(),
+            power: battery::units::Power::default(),
+            energy: battery::units::Energy::default(),
+            time_to_empty: battery::units::Time::default(),
+            time_to_full: battery::units::Time::default(),
+            cycle_count: u32::default(),
+            model: Default::default(),
+            serial_number: Default::default(),
+            state_of_health: Default::default(),
+            state: Default::default(),
+            technology: Default::default(),
+            temperature: Default::default(),
+            voltage: Default::default(),
+            power_bar: VecDeque::with_capacity(super::BUF_CAPACITY),
+            power_bar_y_max: 0.1,
+            power_bar_y_min: -0.1,
+        }
+    }
+
+    pub fn on_tick(&mut self) {
+        let manager = battery::Manager::new().expect("Expected battery manager");
+        for (_idx, maybe_battery) in manager
+            .batteries()
+            .expect("Expected at least one battery")
+            .take(1)
+            .enumerate()
+        {
+            let battery = maybe_battery.expect("Expected battery");
+            self.percentage = battery
+                .state_of_charge()
+                .get::<battery::units::ratio::percent>() as f64
+                / 100.0;
+            self.vendor = battery.vendor().unwrap_or_default().to_owned();
+            self.power = battery.energy_rate();
+            self.energy = battery.energy();
+            self.time_to_empty = battery.time_to_empty().unwrap_or_default();
+            self.time_to_full = battery.time_to_full().unwrap_or_default();
+            self.temperature = battery.temperature().unwrap_or_default();
+            self.voltage = battery.voltage();
+            self.cycle_count = battery.cycle_count().unwrap_or_default();
+            self.model = battery.model().unwrap_or_default().to_owned();
+            self.serial_number = battery.serial_number().unwrap_or("Unknown").to_owned();
+            self.state = battery.state();
+            self.state_of_health = battery
+                .state_of_health()
+                .get::<battery::units::ratio::percent>()
+                / 100.0;
+            self.technology = battery.technology();
+
+            let signed_power = match self.state {
+                battery::State::Charging => {
+                    self.power.get::<battery::units::power::watt>() as f64 * -1.0
+                }
+                _ => self.power.get::<battery::units::power::watt>() as f64,
+            };
+
+            match self.power_bar.len() {
+                _l @ 0..=super::UPPER_INDEX => self.power_bar.push_back(signed_power),
+                _ => {
+                    self.power_bar.pop_front();
+                    self.power_bar.push_back(signed_power);
+                }
+            }
+
+            self.power_bar_y_max = self
+                .power_bar
+                .iter()
+                .max_by(|&l, &r| l.partial_cmp(r).unwrap_or(std::cmp::Ordering::Equal))
+                .unwrap_or(&f64::default())
+                .to_owned();
+            self.power_bar_y_min = self
+                .power_bar
+                .iter()
+                .min_by(|&l, &r| l.partial_cmp(r).unwrap_or(std::cmp::Ordering::Equal))
+                .unwrap_or(&f64::default())
+                .to_owned();
+        }
+    }
+
+    pub fn get_power_bar_grid(&self) -> Vec<(f64, f64)> {
+        self.power_bar
+            .iter()
+            .rev()
+            .enumerate()
+            .map(|(idx, &y)| {
+                let x = super::BUF_CAPACITY - 1 - idx;
+                (x as f64, y)
+            })
+            .collect()
+    }
+}
---
+++ src/lib.rs
@@ -0,0 +1,204 @@
+use crossterm::event::{self, Event, KeyCode};
+use ratatui::{prelude::*, widgets::*};
+use std::{
+    io,
+    time::{Duration, Instant},
+};
+
+const BUF_CAPACITY: usize = 200;
+const UPPER_INDEX: usize = BUF_CAPACITY - 1;
+const GRAPH_CLEARANCE: f64 = 1.0;
+
+mod app;
+pub use app::App;
+
+pub fn run_app<B: Backend>(
+    terminal: &mut Terminal<B>,
+    mut app: App,
+    tick_rate: Duration,
+) -> io::Result<()> {
+    let mut last_tick = Instant::now();
+    loop {
+        terminal.draw(|f| ui(f, &app))?;
+
+        let timeout = tick_rate
+            .checked_sub(last_tick.elapsed())
+            .unwrap_or_else(|| Duration::from_secs(0));
+        if crossterm::event::poll(timeout)? {
+            if let Event::Key(key) = event::read()? {
+                if let KeyCode::Char('q') = key.code {
+                    return Ok(());
+                }
+            }
+        }
+        if last_tick.elapsed() >= tick_rate {
+            app.on_tick();
+            last_tick = Instant::now();
+        }
+    }
+}
+
+fn ui<B: Backend>(f: &mut Frame<B>, app: &App) {
+    // main frame
+    f.render_widget(Block::default().borders(Borders::ALL), f.size());
+
+    let layout = Layout::default()
+        .direction(Direction::Vertical)
+        .margin(1)
+        .constraints(
+            [
+                Constraint::Length(1),
+                Constraint::Max(15),
+                Constraint::Max(3),
+                // Constraint::Percentage(50),
+                Constraint::Min(20),
+            ]
+            .as_ref(),
+        )
+        .split(f.size());
+    let title_area = layout[0];
+
+    f.render_widget(
+        Paragraph::new("Battery info. Press 'q' to quit")
+            .dark_gray()
+            .alignment(Alignment::Center),
+        title_area,
+    );
+
+    let label = Span::styled(
+        format!("{:.2}%", app.percentage * 100.0),
+        Style::default()
+            // .fg(Color::Green)
+            .add_modifier(Modifier::SLOW_BLINK | Modifier::BOLD),
+    );
+
+    let gauge = Gauge::default()
+        .block(Block::default().title("Percentage").borders(Borders::ALL))
+        .gauge_style(Style::default().fg(Color::White))
+        .ratio(app.percentage)
+        .label(label)
+        .use_unicode(true);
+
+    // f.render_widget(gauge.block(block), layout[2]);
+    f.render_widget(gauge, layout[2]);
+
+    let text = vec![
+        Line::from(format!("Model - {}", app.model)),
+        Line::from(format!("Serial - {}", app.serial_number)),
+        Line::from(format!("Technology - {}", app.technology)),
+        Line::from(format!("State - {}", app.state)),
+        Line::from(format!("Health - {:.2}%", app.state_of_health * 100.0)),
+        Line::from(format!("Cycles - {}", app.cycle_count)),
+        Line::from(""),
+        Line::from(format!(
+            "Voltage - {:.3} Volts",
+            app.voltage
+                .get::<battery::units::electric_potential::volt>()
+        )),
+        Line::from(format!(
+            "Power - {} W",
+            app.power.get::<battery::units::power::watt>()
+        )),
+        Line::from(format!(
+            "Energy - {} W/h",
+            app.energy.get::<battery::units::energy::watt_hour>()
+        )),
+        Line::from(format!(
+            "Time to empty - {:.2}h({:.2}m)",
+            app.time_to_empty.get::<battery::units::time::hour>(),
+            app.time_to_empty.get::<battery::units::time::minute>()
+        )),
+        Line::from(format!(
+            "Time to full - {:.2}h({:.2}m)",
+            app.time_to_full.get::<battery::units::time::hour>(),
+            app.time_to_full.get::<battery::units::time::minute>()
+        )),
+        Line::from(format!(
+            "Temperature - {}",
+            app.temperature
+                .get::<battery::units::thermodynamic_temperature::degree_celsius>()
+        )),
+    ];
+
+    let paragraph = Paragraph::new(text).wrap(Wrap { trim: false });
+    f.render_widget(
+        paragraph.block(
+            Block::default()
+                .borders(Borders::ALL)
+                .title(format!("Battery - {}", &app.vendor)),
+        ),
+        layout[1],
+    );
+
+    let x_axis: Vec<_> = (0..BUF_CAPACITY).map(|i| (i as f64, 0.0)).collect();
+
+    let graph_data = &app.get_power_bar_grid();
+    let datasets = vec![
+        Dataset::default()
+            .name("Power draw")
+            .marker(symbols::Marker::Braille)
+            .style(Style::default().fg(Color::LightGreen))
+            .graph_type(GraphType::Line)
+            .data(graph_data),
+        Dataset::default()
+            .name("X axis")
+            .marker(symbols::Marker::Dot)
+            .dark_gray()
+            .graph_type(GraphType::Line)
+            .data(&x_axis),
+    ];
+
+    // Leftmost value of the graph buffer
+    let left_label = app.power_bar.front().unwrap_or(&(0.0));
+    // Average
+    let mid_label = app.power_bar.iter().sum::<f64>() / app.power_bar.len() as f64;
+    // Current value
+    let right_label = app.power_bar.back().unwrap_or(&(0.0));
+
+    let x_labels = vec![
+        Span::styled(
+            format!("{:.2}", left_label),
+            Style::default().add_modifier(Modifier::BOLD),
+        ),
+        Span::raw(format!("AVG:{:.2}", mid_label)),
+        Span::styled(
+            format!("CUR:{:.2}", right_label),
+            Style::default().add_modifier(Modifier::BOLD),
+        ),
+    ];
+
+    let upper_bound = app.power_bar_y_max + GRAPH_CLEARANCE;
+    let lower_bound = if app.power_bar_y_min > 0.0 {
+        0.0
+    } else {
+        app.power_bar_y_min - GRAPH_CLEARANCE
+    };
+    let median = (upper_bound + lower_bound) / 2.0;
+
+    let chart = Chart::new(datasets)
+        .block(
+            Block::default()
+                .title("Power".cyan().bold())
+                .borders(Borders::ALL),
+        )
+        .x_axis(
+            Axis::default()
+                .title("Time(s)")
+                .style(Style::default().fg(Color::Gray))
+                .bounds([0.0, BUF_CAPACITY as f64])
+                .labels(x_labels),
+        )
+        .y_axis(
+            Axis::default()
+                .title("Power(W)")
+                .style(Style::default().fg(Color::Gray))
+                .bounds([lower_bound, upper_bound])
+                .labels(vec![
+                    format!("{:.2}", lower_bound).into(),
+                    format!("{:.2}", median).into(),
+                    format!("{:.2}", upper_bound).into(),
+                ]),
+        );
+
+    f.render_widget(chart, layout[3]);
+}
